d3f5aef90a5a805883c2b5beac26c74fc6eb03a5
vamoosev <vamoosev@gmail.com>
2023-02-27 23:20:43 +0200

    KMS

--- src/scripts/raaputin.py
+++ src/scripts/raaputin.py
@@ -6,6 +6,9 @@ from markdownify import markdownify as md
 import os
 from urllib.parse import urlparse
 import re as re
+import threading
+
+import urllib
 
 SCROLL_PAUSE_TIME = 2
 links: str = []
@@ -22,12 +25,48 @@ elems = driver.find_elements(By.CLASS_NAME ,"O16KGI")
 for elem in elems:
     links.append(elem.get_attribute("href"))
 
+driver.close()
 
 for link in links:
     print(link)
     page:requests.Response = requests.get(link)
-    html:str = page.text
-    md: str = md(html)
-    parsed = re.search("(?<=https:\/\/www.huhuu.news\/blog\/posts\/)(?s)(.*$)", urlparse(link).path)
-    os.open(f"{parsed}.md", os.O_CREAT | os.O_WRONLY)
-    os.write(link, md)
+    html = page.text
+    mdd = md(html)
+    try:
+        ddd = mdd.rsplit(""" --ricos-custom-p-font-weight: inherit;
+ --ricos-custom-p-font-size: 18px;
+ --ricos-custom-p-line-height: 1.5;
+ }""")[1]
+    except:
+        ddd =mdd
+    fff = ddd
+    print(fff)
+    parsed = urllib.parse.unquote(link).split("/")[-1]
+    kaka = fff.replace(r"""* [Ajankohtaista](https://www.huhuu.news/blog/categories/ajankohtaista)
+    [toimitus@huhuu.news](mailto:toimitus@huhuu.news)
+
+* [![Instagram]()](https://www.instagram.com/huhuu.news/)
+* [![YouTube]()](https://www.youtube.com/channel/UCAAg6JGz08V5XoFkTcEQOtg)
+bottom of page
+
+ window.firstPageId = 'jykqx'
+ window.bi.sendBeat(12, 'Partially visible', {pageId: window.firstPageId})
+ if (window.requestCloseWelcomeScreen) {
+ window.requestCloseWelcomeScreen()
+ }
+ if (!window.\_\_browser\_deprecation\_\_) {
+ window.fedops.phaseStarted('partially\_visible', {paramsOverrides: { pageId: firstPageId }})
+ }
+
+
+
+
+
+
+{"appsWarmupData":{},"platform":{"ssrPropsUpdates":[],"ssrStyleUpdates":[],"ssrStructureUpdates":[]},"ooi":{"failedInSsr":{}}}""","")
+    print(f"{parsed}.md")
+    ffff = f"""---\nlayout: "../../layouts/NewsLayout.astro"\n---{kaka}"""
+    faa= open(f"{parsed}.md","x")
+    print(faa)
+    faa.write(ffff)
+    faa.close()
